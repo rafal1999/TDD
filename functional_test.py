4d016db567e8d6cea7132f11c73afa1f3edf8955
rafal1999 <ravstb4@gmail.com>
2021-04-14 20:59:40 +0200

    stepforward

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,22 @@
 
 from  selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(10)
+    def tearDown(self):
+        self.browser.quit()
+    
+    def test_can_a_list(self):
+        self.browser.get("http://localhost:8000")
+        self.assertIn('nic',self.browser.title)
+        # self.fail("end test")
 
-assert 'Django' in browser.title
+
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
+
+
