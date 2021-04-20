d5867b6a4cbb760d9032b71762f762beacb038a3
rafal1999 <ravstb4@gmail.com>
2021-04-20 18:27:50 +0200

    Front page html now genereted from a template

--- functional_test.py
+++ functional_test.py
@@ -13,21 +13,21 @@ class NewVisitorTest(unittest.TestCase):
     
     def test_can_a_list(self):
         self.browser.get("http://localhost:8000")
-        self.assertIn('To do list',self.browser.title)
+        self.assertIn('Todo list',self.browser.title)
         header_text=self.browser.find_element_by_tag_name('h1').text
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),'Enter a todo item'
         )
-        inputbox.send_keys('Buy car')
-
-        inputbox.send_keys(Keys.ENTER)
+        inputbox.send_keys('Buy a car')
         time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(5)
 
         table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_element_by_tag_name('tr')
+        rows=table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: buy a car ' for row in rows)
+            any(row.text == '1: buy a car ' for row in rows), 'you don`t buy a car xd'
         )
         self.fail("end test")
 
