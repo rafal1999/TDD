526cabf293e37a73c2f27e8dd57617895c108bd6
rafal1999 <ravstb4@gmail.com>
2021-04-21 14:39:38 +0200

    before ORM

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
     
+    def check_for_row_in_list_table(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_a_list(self):
         self.browser.get("http://localhost:8000")
         self.assertIn('Todo list',self.browser.title)
@@ -29,12 +34,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         table=self.browser.find_element_by_id('id_list_table')
         rows=table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy a car' for row in rows)
-        )
-        self.assertTrue(
-            any(row.text == '2: Buy a pen' for row in rows)
-        )
+        self.check_for_row_in_list_table('1: Buy a car')
+        self.check_for_row_in_list_table('2. Buy a pen')
         self.fail("END TEST")
 
 
