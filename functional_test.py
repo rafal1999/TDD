5234432a6c2a89f22caae710023cac3dfa60e3b0
rafal1999 <ravstb4@gmail.com>
2021-04-27 13:29:14 +0200

    Rediredt after POST, and show all items in template

--- functional_test.py
+++ functional_test.py
@@ -22,8 +22,7 @@ class NewVisitorTest(unittest.TestCase):
         header_text=self.browser.find_element_by_tag_name('h1').text
         inputbox=self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-            inputbox.get_attribute('placeholder'),'Enter a todo item'
-        )
+            inputbox.get_attribute('placeholder'),'Enter a todo item')
         inputbox.send_keys('Buy a car') 
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
--- lists/tests.py
+++ lists/tests.py
@@ -2,8 +2,8 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
-from lists.models import Item
 from lists.views import home_page
+from lists.models import Item
 # Create your tests here.
 
 class HomePageTest(TestCase):
@@ -21,10 +21,30 @@ class HomePageTest(TestCase):
         self.assertTrue(html.endswith('</html>'))
 
     def test_home_page_can_save_a_POST_request(self):
-         response = self.client.post('/', data={'item_text': 'A new list item'})
-         self.assertIn('A new list item',  response.content.decode()), "no nie działa"
-         self.assertTemplateUsed(response, 'home.html')
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')     
+        # self.assertEqual(response.status_code,302)
+        # self.assertEqual(response['location'], '/')      
+        #self.assertIn('A new list item',  response.content.decode()), "no nie działa"
+        # self.assertTemplateUsed(response, 'hoe.html')
     
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
+
+    def test_displays_all_list_item(self):
+        Item.objects.create(text='item 1')
+        Item.objects.create(text='item 2')
+        
+        response = self.client.get('/')
+
+        self.assertIn('item 1', response.content.decode())
+        self.assertIn('item 2', response.content.decode())
+
 class ItemModelTest(TestCase):
     
     def test_saving_and_retrieving_items(self):
